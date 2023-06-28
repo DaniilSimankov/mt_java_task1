ef0a169d14487aa525b36bd8b419937ab4e40f73
Daniel <thedanils63@mail.ru>
2023-06-28 23:35:11 +0300

    added JavaDoc for Controller and Service

--- src/main/java/com/example/maximtechnology/controllers/TaskController.java
+++ src/main/java/com/example/maximtechnology/controllers/TaskController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * The TaskController class is responsible for handling HTTP requests related to tasks.
+ */
 @RestController
 @RequestMapping("/api/tasks")
 @RequiredArgsConstructor
@@ -17,6 +20,12 @@ public class TaskController {
 
     private final TaskService taskService;
 
+    /**
+     * Creates a new task.
+     *
+     * @param task The task to create.
+     * @return A success message.
+     */
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
     public String createTask(@RequestBody Task task) {
@@ -24,6 +33,12 @@ public class TaskController {
         return "The task was created";
     }
 
+    /**
+     * Retrieves tasks for today.
+     *
+     * @param completed The completion status to filter tasks (optional).
+     * @return A list of task DTOs for today.
+     */
     @GetMapping("/today")
     @ResponseStatus(HttpStatus.OK)
     public List<TaskDto> getTasksForToday(@RequestParam(name = "completed", required = false) Boolean completed) {
@@ -33,6 +48,12 @@ public class TaskController {
         return taskService.getTasksForTodayByCompleted(completed);
     }
 
+    /**
+     * Retrieves tasks for the current week.
+     *
+     * @param completed The completion status to filter tasks (optional).
+     * @return A list of task DTOs for the week.
+     */
     @GetMapping("/week")
     @ResponseStatus(HttpStatus.OK)
     public List<TaskDto> getTasksForWeek(@RequestParam(name = "completed", required = false) Boolean completed) {
@@ -42,6 +63,12 @@ public class TaskController {
         return taskService.getTasksForWeekByCompleted(completed);
     }
 
+    /**
+     * Retrieves tasks for the current month.
+     *
+     * @param completed The completion status to filter tasks (optional).
+     * @return A list of task DTOs for the month.
+     */
     @GetMapping("/month")
     @ResponseStatus(HttpStatus.OK)
     public List<TaskDto> getTasksForMonth(@RequestParam(name = "completed", required = false) Boolean completed) {
@@ -51,6 +78,13 @@ public class TaskController {
         return taskService.getTasksForMonthByCompleted(completed);
     }
 
+    /**
+     * Updates a task by its ID.
+     *
+     * @param id   The ID of the task to update.
+     * @param task The updated task.
+     * @return The updated task DTO.
+     */
     @PostMapping("/{id}/update")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public TaskDto updateTaskById(@PathVariable("id") Long id, @RequestBody Task task) {
@@ -58,6 +92,12 @@ public class TaskController {
         return taskService.updateTaskById(task);
     }
 
+    /**
+     * Deletes a task by its ID.
+     *
+     * @param id The ID of the task to delete.
+     * @return A success message.
+     */
     @PostMapping("/{id}/delete")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public String deleteTaskById(@PathVariable("id") Long id) {
@@ -66,6 +106,13 @@ public class TaskController {
         return "Task with id " + id + " was deleted";
     }
 
+    /**
+     * Updates the completion status of a task.
+     *
+     * @param id        The ID of the task to update.
+     * @param completed The new completion status of the task (optional, default is true).
+     * @return The updated task DTO.
+     */
     @PostMapping("/{id}/completed")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public TaskDto updateTaskStatus(@PathVariable("id") Long id, @RequestParam(name = "completed", required = false) Boolean completed){
--- src/main/java/com/example/maximtechnology/services/TaskService.java
+++ src/main/java/com/example/maximtechnology/services/TaskService.java
@@ -5,24 +5,84 @@ import com.example.maximtechnology.models.Task;
 
 import java.util.List;
 
+/**
+ * The TaskService interface provides methods for managing tasks.
+ */
 public interface TaskService {
+    /**
+     * Creates a new task.
+     *
+     * @param task The task to create.
+     * @return The created task DTO.
+     */
     TaskDto createTask(Task task);
 
+    /**
+     * Retrieves all tasks for today.
+     *
+     * @return A list of task DTOs for today.
+     */
     List<TaskDto> getAllTasksForToday();
 
+    /**
+     * Retrieves all tasks for the current week.
+     *
+     * @return A list of task DTOs for the week.
+     */
     List<TaskDto> getAllTasksForWeek();
 
+    /**
+     * Retrieves all tasks for the current month.
+     *
+     * @return A list of task DTOs for the month.
+     */
     List<TaskDto> getAllTasksForMonth();
 
+    /**
+     * Retrieves tasks for today based on the completion status.
+     *
+     * @param completed The completion status to filter tasks.
+     * @return A list of task DTOs for today filtered by completion status.
+     */
     List<TaskDto> getTasksForTodayByCompleted(boolean completed);
 
+    /**
+     * Retrieves tasks for the current week based on the completion status.
+     *
+     * @param completed The completion status to filter tasks.
+     * @return A list of task DTOs for the week filtered by completion status.
+     */
     List<TaskDto> getTasksForWeekByCompleted(boolean completed);
 
+    /**
+     * Retrieves tasks for the current month based on the completion status.
+     *
+     * @param completed The completion status to filter tasks.
+     * @return A list of task DTOs for the month filtered by completion status.
+     */
     List<TaskDto> getTasksForMonthByCompleted(boolean completed);
 
+    /**
+     * Updates a task by its ID.
+     *
+     * @param task The updated task.
+     * @return The updated task DTO.
+     */
     TaskDto updateTaskById(Task task);
 
+    /**
+     * Deletes a task by its ID.
+     *
+     * @param id The ID of the task to delete.
+     */
     void deleteTaskById(Long id);
 
+    /**
+     * Updates the completion status of a task.
+     *
+     * @param id          The ID of the task to update.
+     * @param isCompleted The new completion status of the task.
+     * @return The updated task DTO.
+     */
     TaskDto updateTaskStatus(Long id, boolean isCompleted);
 }
